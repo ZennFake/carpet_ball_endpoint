438411537bfebfd2f45f5e00f250ed943404faed
ZenDev <76600019+ZennFake@users.noreply.github.com>
2026-02-22 14:02:47 -0700

    dnjwoajdosa

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771793808|4078631184';
+const CACHE_VERSION = '1771794145|4415611439';
 /** @type {string} */
 const CACHE_PREFIX = 'client-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
