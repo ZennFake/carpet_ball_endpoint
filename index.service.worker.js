5b343a6f231ec4f8683a4a80d7947c0d90cf2eef
ZenDev <76600019+ZennFake@users.noreply.github.com>
2026-02-22 20:26:54 -0700

    please work

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771816877|801195793';
+const CACHE_VERSION = '1771817190|1114168194';
 /** @type {string} */
 const CACHE_PREFIX = 'Carpet Ball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
