79cd29e3a1747e851b0e01f6d86f93ff9b0971da
ZenDev <76600019+ZennFake@users.noreply.github.com>
2026-02-22 21:25:24 -0700

    dwasdwa

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,14 +4,14 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771820047|3971453856';
+const CACHE_VERSION = '1771820705|4629501649';
 /** @type {string} */
 const CACHE_PREFIX = 'Carpet Ball-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
 const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
-const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
+const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = false;
 // Files that will be cached on load.
 /** @type {string[]} */
 const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
